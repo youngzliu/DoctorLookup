0f1d796d2a229f7db8ef203408d1f83ad5b265ae
Young Liu <youngliu92@gmail.com>
2019-03-29 16:46:32 -0700

    add table structure for results.

--- src/doctorLookup.js
+++ src/doctorLookup.js
@@ -2,7 +2,7 @@ export class DoctorLookup{
   getDoctorInfo(condition, name, skip = 0){
     return new Promise(function(resolve, reject){
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${condition}&user_location=45.52%2C%20-122.68&sort=distance-asc&skip=${skip}&limit=10&user_key=${process.env.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${condition}&user_location=45.52%2C%20-122.68&sort=distance-asc&skip=${skip}&limit=5&user_key=${process.env.apiKey}`;
       request.onload = function(){
         if(this.status === 200){
           resolve(request.response);
