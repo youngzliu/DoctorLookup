486901acbbcd9fcb01baf6e09298160b1771346b
Young Liu <youngliu92@gmail.com>
2019-03-29 16:19:19 -0700

    add basic structure for parsing user input.

--- src/main.js
+++ src/main.js
@@ -2,15 +2,25 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-import { pingPong } from './ping-pong';
+import { DoctorLookup } from './doctorLookup.js';
 
 $(document).ready(function() {
-  $('#ping-pong-form').submit(function(event) {
+  let name;
+  let condition;
+  let skip;
+  $('#doctorForm').submit(function(event) {
     event.preventDefault();
-    var goal = $('#goal').val();
-    var output = pingPong(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
+    condition = $("#condition").val();
+    name = $("#doctorName").val();
+    if(condition === "" && name === ""){
+      alert("Please fill in at least one of the fields!");
+    }
+    else{
+      let doctorLookup = new DoctorLookup();
+      doctorLookup.getDoctorInfo(condition, name).then(function(response){
+        let body = JSON.parse(response);
+        console.log(body);
+      });
+    }
   });
 });
