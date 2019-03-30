6b4e2ed016451729d90e88f737f38ff2b20016c2
Young Liu <youngliu92@gmail.com>
2019-03-29 21:59:56 -0700

    add functionality to display results to table for user input.

--- src/doctorLookup.js
+++ src/doctorLookup.js
@@ -1,12 +1,8 @@
 export class DoctorLookup{
-  getDoctorInfo(condition, name, distance, skip = 0){
+  getDoctorInfo(condition, name, distance, perPage, skip = 0){
     return new Promise(function(resolve, reject){
       let request = new XMLHttpRequest();
-      if(distance === ""){
-        distance = 1;
-      }
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${condition}&location=45.52%2C%20-122.68%2C%20${distance}&user_location=45.52%2C%20-122.68&sort=distance-asc&skip=${skip}&limit=5&user_key=${process.env.apiKey}`
-      console.log(url);
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${condition}&location=45.52%2C%20-122.68%2C%20${distance}&user_location=45.52%2C%20-122.68&sort=distance-asc&skip=${skip}&limit=${perPage}&user_key=${process.env.apiKey}`;
       request.onload = function(){
         if(this.status === 200){
           resolve(request.response);
@@ -18,5 +14,15 @@ export class DoctorLookup{
       request.open("GET", url, true);
       request.send();
     });
+  }
+
+  getClosestPractice(practices){
+    let closest = practices[0];
+    practices.forEach(function(practice){
+      if(practice.distance < closest.distance){
+        closest = practice;
+      }
+    });
+    return closest;
   }
 }
--- src/main.js
+++ src/main.js
@@ -9,27 +9,67 @@ $(document).ready(function() {
   let condition;
   let skip;
   let distance;
+  const perPage = 10;
+  let doctorLookup = new DoctorLookup();
   $('#doctorForm').submit(function(event) {
     event.preventDefault();
     condition = $("#condition").val();
     name = $("#doctorName").val();
     distance = $("#distance").val();
+    if(distance === ""){
+      distance = "50";
+    }
     if(condition === "" && name === ""){
       $(".invalid-feedback").show();
       $("#condition").addClass("is-invalid");
       $("#doctorName").addClass("is-invalid");
     }
     else{
+      $("#results").hide();
+      $("#results").empty();
+      $("#error").empty();
       $(".invalid-feedback").hide();
       $("#condition").removeClass("is-invalid");
       $("#doctorName").removeClass("is-invalid");
-      let doctorLookup = new DoctorLookup();
-      doctorLookup.getDoctorInfo(condition, name, distance).then(function(response){
+      doctorLookup.getDoctorInfo(condition, name, distance, perPage).then(function(response){
         let body = JSON.parse(response);
         console.log(body);
+        if(body.meta.total == 0){
+          $("#results").prepend(`<h3>There were unfortunately no doctors that matched you requirements. Sorry!</h3>`);
+          $("#results").show();
+        }
+        else{
+          $("#results").append(`<h3>We have found ${body.meta.total} doctors within ${distance} miles that match your query. Here they are, sorted by how far they are from you. Information is based on their practice closest to your location.</h3>`);
+          $("#results").append("<table id='resultsTable'><tr><th>Name</th><th>Closest Practice</th><th>Address</th><th>Phone Number</th><th>Website</th><th>Accepting new patients?</th><th>Distance from you</th></tr></table>");
+          body.data.forEach(function(doctor){
+            let practice = doctorLookup.getClosestPractice(doctor.practices);
+            let rowString = ""
+            rowString += `<tr><td>${doctor.profile.first_name} ${doctor.profile.last_name}</td>`;
+            let website = practice.website;
+            if(website === undefined)
+            { website = "none"; }
+            let accept = practice.accepts_new_patients;
+            if(accept === true)
+            { accept = "Yes"; }
+            else
+            { accept = "No"; }
+            rowString += `<td>${practice.name}</td>`;
+            rowString += `<td>${practice.visit_address.street}, ${practice.visit_address.city} ${practice.visit_address.state}</td>`;
+            rowString += `<td>${practice.phones[0].number}</td>`;
+            rowString += `<td>${website}</td>`;
+            rowString += `<td>${accept}</td>`;
+            rowString += `<td>${parseFloat(practice.distance).toFixed(2)} miles</tr>`
+            $("#resultsTable").append(rowString);
+          });
+          $("#results").show();
+        }
       }, function(error){
-
+        $('#error').html(`Something went wrong processing you request! Here is the error message: ${error.message}`);
       });
     }
   });
+
+  function updateButtons(){
+
+  }
 });
